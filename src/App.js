ef3bd59a0f077a44e3d966b78da941318d696855
paulballas <paul.ballas@gmail.com>
2019-03-02 09:24:27 -0700

    iniitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './sass/app.sass'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-xs-12'>
+              <p>Hello World!</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
