20b0f4f00d657cef9ab644883085743883420d34
paulballas <paul.ballas@gmail.com>
2019-03-02 19:25:51 -0700

    home wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import bear from './bear.svg'
+import logos from './sd-logos.svg'
 import './sass/app.sass'
 
 class App extends Component {
   render() {
     return (
       <div>
+
         <div className='container-fluid header home-hero'>
           <div className='row middle-xs between-xs'>
             <div className='col-md-2 col-md-offset-0 col-xs-8 col-xs-offset-2'>
@@ -33,7 +35,7 @@ class App extends Component {
                 <a className='btn btn-purple'>Start The Conversation</a>
               </div>
             </div>
-            <div className='col-md-6'>
+            <div className='col-md-6 p-t-7-md p-t-0-xs'>
               <img
                 src={bear}
                 className='img-default blue-bear'
@@ -42,6 +44,25 @@ class App extends Component {
             </div>
           </div>
         </div>
+
+        <div className='container-fluid home-services pad-section'>
+          <div className='row'>
+            <div className='col-md-6 col-xs-12'>
+              <p className='section'>services</p>
+              <h3>Logo</h3>
+              <p className='bold large p-r-4-md p-r-0-xs'>You need a logo that stands out from the crowd that doesn't cost a fortune. We'll quickly create a logo for use anywhere it's needed.</p>
+            </div>
+            <div className='col-md-4 col-xs-12'>
+              <img
+                src={logos}
+                className='img-default'
+                alt='Bird Logo'
+                />
+            </div>
+            <div className='col-md-2 col-xs-12'></div>
+          </div>
+        </div>
+
       </div>
     );
   }
